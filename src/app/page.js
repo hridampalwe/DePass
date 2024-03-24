049166079cd4278969a9fb147f79d9f5b5bb24fd
Hridam Palwe <palwehridam@gmail.com>
2024-03-24 12:42:00 +0530

    Added Dashboard and Login View

--- src/app/page.js
+++ src/app/page.js
@@ -39,7 +39,8 @@ import {
   Box,
   Flex,
 } from "@chakra-ui/react";
-import logo from "../../public/blockChain.png";
+import Login from "./Login.js";
+import Dashboard from "./Dashboard.js";
 //Fetch the contract address from env.
 const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
 //Using the contract ABI from the compiled contract.
@@ -617,74 +618,8 @@ export default function Home() {
 
   return (
     <ChakraProvider>
-      <Flex
-        width={"100vw"}
-        height={"100vh"}
-        alignContent={"center"}
-        justifyContent={"center"}
-        bgGradient="linear(red.100 0%, orange.100 25%, yellow.100 50%)"
-      >
-        <Center>
-          <Card
-            direction={{ base: "column", sm: "row" }}
-            overflow="hidden"
-            variant="elevated"
-            boxShadow="dark-lg"
-            p="6"
-            rounded="md"
-            bg="white"
-          >
-            <Image
-              objectFit="cover"
-              maxW={{ base: "100%", sm: "300px" }}
-              src="blockChain.png"
-              alt="Caffe Latte"
-            />
-            <Stack>
-              <CardBody py="10">
-                <Heading
-                  // style={{ color: "#A370AF" }}
-                  //
-                  size="xl"
-                  bgGradient="linear(to-l, #7928CA, #FF0080)"
-                  bgClip="text"
-                >
-                  DePass: Decentralised Password Manager
-                </Heading>
-
-                <Text
-                  py="2"
-                  pt="4"
-                  style={{ fontSize: "20px", fontWeight: "bold" }}
-                >
-                  A blockchain based password manager to securely store your
-                  credentials.
-                </Text>
-                <Center>
-                  <Stack pt="5%" direction="row" spacing={4}>
-                    <Button
-                      // leftIcon={<EmailIcon />}
-                      colorScheme="teal"
-                      variant="solid"
-                      onClick={handleConnectWallet}
-                    >
-                      Login using MetaMask
-                    </Button>
-                    <Button
-                      // rightIcon={<ArrowForwardIcon />}
-                      colorScheme="teal"
-                      variant="outline"
-                    >
-                      Download MetaMask
-                    </Button>
-                  </Stack>
-                </Center>
-              </CardBody>
-              {/* <CardFooter></CardFooter> */}
-            </Stack>
-          </Card>
-        </Center>
-      </Flex>
+      {!provider && <Login handleConnectWallet={handleConnectWallet} />}
+      {provider && <Dashboard />}
     </ChakraProvider>
   );
 }
