4d0092e1dbdef9bd254bb77e01b06d0bd670409e
Hridam Palwe <palwehridam@gmail.com>
2024-03-25 02:37:14 +0530

    Added DePass Logo

--- src/app/Sidebar.js
+++ src/app/Sidebar.js
@@ -9,6 +9,7 @@ import {
   Drawer,
   DrawerContent,
   useDisclosure,
+  Image,
 } from "@chakra-ui/react";
 import {
   FiHome,
@@ -59,7 +60,7 @@ export default function SimpleSidebar() {
 const SidebarContent = ({ onClose, ...rest }) => {
   return (
     <Box
-      bg={useColorModeValue("white", "gray.900")}
+      // bg={useColorModeValue("white", "gray.900")}
       //   borderRight="1px"
       //   borderRightColor={useColorModeValue("gray.200", "gray.700")}
       w={{ base: "full", md: 60 }}
@@ -68,9 +69,7 @@ const SidebarContent = ({ onClose, ...rest }) => {
       {...rest}
     >
       <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
-        <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-          Logo
-        </Text>
+        <Image src="blockChain.png" />
         <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
       </Flex>
       {LinkItems.map((link) => (
